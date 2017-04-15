f4cdba5dba6fccbf0ac4c904200478be1c86b8e6
Palle <palleklewitz@icloud.com>
2017-04-16 00:44:39 +0200

    Activated DB Added admin column to user. Added verify function to user, which verifies login credentials Passwords are now encrypted using bcrypt.

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import bcrypt
+
 # from sqlalchemy import Column, Integer, String
 from app import db
 
@@ -13,19 +15,31 @@ Base.query = db_session.query_property()
 
 # Set your classes here.
 
-'''
+
 class User(Base):
     __tablename__ = 'Users'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(30))
+    password = db.Column(db.String(64))  # bcrypt requires 59 or 60 characters
+    admin = db.Column(db.Bool, default=False)
 
     def __init__(self, name=None, password=None):
         self.name = name
-        self.password = password
-'''
+        self.password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    @staticmethod
+    def verify(username, password):
+        """Returns true, if a user with the specified username and password exists.
+        If no user exists with the given username or the password was incorrectly entered,
+        false is returned.
+        
+        :argument username: Username, which should be checked.
+        :argument password: Password, which should be checked.
+        """
+        user = User.query.filter_by(username=username).first()
+        return bcrypt.checkpw(password, user.password)
 
 class Transaction(Base):
     __tablename__ = 'Transactions'
