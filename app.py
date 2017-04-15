69c80d0cff977755d501b77b9f2524916e662b01
Benjamin Reich <Benni-Reich@hotmail.de>
2017-04-15 21:36:36 +0200

    use boilerplate

--- app.py
+++ app.py
@@ -1,40 +1,107 @@
-from flask import Flask, render_template, send_from_directory
-from flask_sqlalchemy import SQLAlchemy
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request
+# from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from forms import *
 import os
 
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/dionysos'
+app.config.from_object('config')
+#db = SQLAlchemy(app)
 
-# http://flask-sqlalchemy.pocoo.org/2.1/quickstart/
-db = SQLAlchemy(app)
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+'''
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+'''
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
 
 
-class Thing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(120), unique=True)
+@app.route('/')
+def home():
+    return render_template('pages/placeholder.home.html')
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-# aber 
-    # def __repr__(self):
-        # return '<Thing %r>' % self.name
 
-@app.route("/")
-def hello():
-  #things = Thing.query.all()
-  # return render_template('index.html', var='Das ist der erste Test', things=things)
-  return render_template('index.html', var='Das ist der erste Test')
-  
-@app.route('/dist/<path:path>')
-def send_path(path):
-   return send_from_directory('dist', path)
-	
-@app.route('/assets/<path:path>')
-def send_assets(path):
-   return send_from_directory('assets', path)
+@app.route('/about')
+def about():
+    return render_template('pages/placeholder.about.html')
 
-if __name__ == "__main__":
-  app.run(host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', 8080), debug=True)
+
+@app.route('/login')
+def login():
+    form = LoginForm(request.form)
+    return render_template('forms/login.html', form=form)
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm(request.form)
+    return render_template('forms/register.html', form=form)
+
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
+# Error handlers.
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
--- models.py
+++ models.py
@@ -8,7 +8,6 @@ engine = create_engine('sqlite:///database.db', echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-
 Base = declarative_base()
 Base.query = db_session.query_property()
 
@@ -17,18 +16,16 @@ Base.query = db_session.query_property()
 '''
 class User(Base):
     __tablename__ = 'Users'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(30))
+
     def __init__(self, name=None, password=None):
-
         self.name = name
-
         self.password = password
 '''
 
-
 # Create tables.
-
 Base.metadata.create_all(bind=engine)
