565d0155013c0c51a514711eac47a72ad1054839
Palle <palleklewitz@icloud.com>
2017-04-18 04:16:08 +0200

    Fixed tests (or not?)

--- routes_tests.py
+++ routes_tests.py
@@ -1,14 +1,18 @@
 import pytest
-import pytest_flask
-import app
-from flask import Flask, url_for
+from flask import url_for
+from app import app as application
 
-@pytest.fixture
+
+@pytest.fixture(scope='function')
 def app():
-    return app.app
+    application.app_context().push()
+    application.config['DEBUG'] = True
+    application.config['TESTING'] = True
+    return application
 
 
-def test_all_routes(app):
+# noinspection PyShadowingNames
+def test_routes(app):
     client = app.test_client()
-    for route in ['/', '/leaderboard', '/transactions', '/login', '/register', '/forgot']:
+    for route in ['home', 'leaderboard', 'transactions', 'login', 'register', 'forgot']:
         assert client.get(url_for(route)).status_code == 200
